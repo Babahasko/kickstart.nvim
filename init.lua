a9be5647b4444235a1332a34c10971f663cca461
Babahasko <zakutni4ek@gmail.com>
2025-01-26 01:37:18 +0300

    remove ast-grep

--- init.lua
+++ init.lua
@@ -661,7 +661,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'ast_grep',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
